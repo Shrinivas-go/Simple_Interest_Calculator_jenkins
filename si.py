23ab07e67d7885bae850ebbb53cf492d2d241d95
Shrinivas <shrinivas4927@gmail.com>
2025-11-27 21:53:19 +0530

    Update si.py

--- si.py
+++ si.py
@@ -3,15 +3,15 @@ import sys
 
 if len(sys.argv) == 4:
     script_name = sys.argv[0]
-    principle = float(sys.argv[1])
-    rate = float(sys.argv[2])
-    time = float(sys.argv[3])
+    principle = int(sys.argv[1])
+    rate = int(sys.argv[2])
+    time = int(sys.argv[3])
 else:
     
     script_name = sys.argv[0]
-    principle = 30000.0
-    rate = 3.0
-    time = 2.0
+    principle = 30000
+    rate = 3
+    time = 2
 
 simple_interest = (principle * rate * time) / 100
 
