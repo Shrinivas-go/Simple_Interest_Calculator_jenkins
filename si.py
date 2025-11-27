8731e6f4b19e3dac228ce71f4de2a80e2c15a5ab
Shrinivas <shrinivas4927@gmail.com>
2025-11-27 21:39:29 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,9 +1,18 @@
 import sys
-if len(sys.argv) < 2:
-    print("Usage: python si.py <principle> <time> <rate>")
-    sys.exit(1)
-principle = int(sys.argv[1])
-time = int(sys.argv[2])   
-rate = int(sys.argv[2])  
-simple_interest = (principle * time * rate) / 100
-print("Simple interest:",simple_interest)
+
+
+if len(sys.argv) == 4:
+    script_name = sys.argv[0]
+    principle = float(sys.argv[1])
+    rate = float(sys.argv[2])
+    time = float(sys.argv[3])
+else:
+    
+    script_name = sys.argv[0]
+    principle = 1000.0
+    rate = 5.0
+    time = 2.0
+
+simple_interest = (principle * rate * time) / 100
+
+print(f"Simple Interest = {simple_interest:.2f}")
